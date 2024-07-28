2a23cec7859de5e90fa5e1d3c490b47ab12fae8a
NicoleCG25 <141361306+NicoleCG25@users.noreply.github.com>
2024-07-28 20:35:44 -0300

    Add files via upload

--- php/formularioUsuario.php
+++ php/formularioUsuario.php
@@ -1,33 +1,24 @@
 <?php
 
-include("conexao.php");
+    include("conexao.php");
 
-$Nome = $_POST['Nome'];
-$Estado = $_POST['Estado'];
-$Cidade = $_POST['Cidade'];
-$Bairro = $_POST['Bairro'];
-$DataNasc = $_POST['DataNasc'];
-$Email = $_POST['Email'];
-$Senha = $_POST['Senha'];
+    $nome = $_POST['nome'];
+    $estado = $_POST['estado'];
+    $cidade = $_POST['cidade'];
+    $bairro = $_POST['bairro'];
+    $dataNasc = $_POST['dataNasc'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
 
-// Prepara a declaração SQL
-$stmt = $mysqli->prepare("INSERT INTO usuario (Nome, Estado, Cidade, Bairro, DataNasc, Email, Senha) VALUES (?, ?, ?, ?, ?, ?, ?)");
-if ($stmt === false) {
-    die("Erro na preparação: " . $mysqli->error);
-}
+    $sql = "INSERT INTO usuario (nome, estado, cidade, bairro, data_nasc, email, senha)
+    VALUES ('$nome', '$estado', '$cidade', '$bairro', $dataNasc, '$email', '$senha')";
 
-// Liga os parâmetros
-$stmt->bind_param("sssssss", $Nome, $Estado, $Cidade, $Bairro, $DataNasc, $Email, $Senha);
-
-// Executa a declaração
 if ($stmt->execute()) {
     echo "<h1>Formulário enviado com sucesso!</h1>";
-} 
-    else {
-        echo "Erro: " . $stmt->error;
-    }
+    
+} else {
+    echo "Erro: " . $sql . "<br>" . $conn->error;
+}
 
-// Fecha a declaração e a conexão
-$stmt->close();
-$mysqli->close();
+$conn->close();
 ?>
